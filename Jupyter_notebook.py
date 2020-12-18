7a72cd2ca18b31fab4632a568c728a4311270494
Kanmegne <40858737+Kanmegne@users.noreply.github.com>
2020-12-18 14:43:16 +0100

    Add files via upload

---
+++ Jupyter_notebook.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook is created for the Capstone project
+
+# In[ ]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[ ]:
+
+
+print("Hello Capstone Project Course!")
+
